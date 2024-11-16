81663f3ff161b9786acf1f7a0cb60ec97bf0252d
Wei He <git@weispot.com>
2024-11-15 21:25:50 -0500

    chore: expose all types

--- mod.ts
+++ mod.ts
@@ -1,8 +1,5 @@
 export { default as createSchedulerApp } from "@src/app.ts";
 
-export type {
-  SchedulerAppOptions,
-  SchedulerJobData,
-} from "@src/utils/types.ts";
+export * from "@src/utils/types.ts";
 
 export { createRepoJobWorker as createWorker } from "@src/worker/create-worker.ts";
